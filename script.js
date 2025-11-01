255d3733440cf882c0346b7bad2fe8f2d26b1b50
Vladyslav_11 <vladyyemets@gmail.com>
2025-11-01 13:46:54 +0200

    jshm1

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1.
+
+// const age = prompt("Скільки вам років?");
+// console.log(age);
+
+// 2.
+
+// const name = prompt('Як вас звати?');
+// console.log(name);
+
+// 3.
+
+// const isStudent = confirm("Ви студент?");
+// console.log(isStudent);
+
+// 4.
+
+// const myString = prompt("Яка ваша улюблена цитата?");
+// console.log(myString);
+
+// 5.
+
+// let myNumber = Number(prompt("Напишіть довільне число"));
+// myNumber = myNumber + 10;
+
+// console.log(myNumber);
+
+// 6.
+
+// const myNull = null;
+// console.log(myNull);
+
+// 7.
+
+// const userName = prompt("Введіть своє ім’я");
+
+// console.log(typeof(userName));
+// alert(userName);
+
+// 8.
+
+// const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(userAnswer);
+
+// 9.
+
+// alert("Увага! Дія може бути небезпечною");
+
+// const answ = confirm("Ви впевнені, що хочете продовжити?");
